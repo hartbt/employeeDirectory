09471c265881441c589ab53fd6100b45b36c358d
Brendon Hart <brendonhartbusiness@gmail.com>
2020-12-08 23:04:08 -0700

    updated the api.js file

--- employeedirectory/src/hooks/api.js
+++ employeedirectory/src/hooks/api.js
@@ -5,19 +5,13 @@ import axios from "axios"
 export function useGet(url){
     const{employees, setEmployees, displayedEmployees, setDisplayedEmployees} = useContext(EmployeeContext)
 
-    // help writing custom hooks and specifically ones that are async
-    // -- https://dev.to/vinodchauhan7/react-hooks-with-async-await-1n9g
-    // -- https://reactjs.org/docs/hooks-custom.html
-    // -- TA Jackson Oppenheim Github: jacksonopp
-   
-    // gets employees from an api, stores them in both employees, and displayed employees. Only occurs once when component is mounted.
     useEffect(()=> {
         async function getEmployees(){
             try {
                 const response = await axios.get(url)
-                // using employees to store a copy of all the employees that will not be manipulated
+
                 setEmployees(response.data.results)
-                // using displayed employees to be a manipulated version of the employees that will be displayed for sorting and filtering.
+
                 setDisplayedEmployees(response.data.results)
             }
             catch (error) {
@@ -27,7 +21,6 @@ export function useGet(url){
         getEmployees()
     },[])
 
-    // used to decide which sort function to trigger
     function sortFunc(sort){
         switch(sort){
             case "name":
@@ -41,7 +34,6 @@ export function useGet(url){
         }
     }
 
-    // sorts the employees based on first name.
     function sortByName(){
          employees.sort(function(a,b){
             if(a.name.first < b.name.first){
@@ -50,11 +42,10 @@ export function useGet(url){
                 return 1;
             }
         })
-        // You have to spread, because this creates a new variable, instead of just updating the variable. React will not recognize it as an update if you just update the variable.
+
         setDisplayedEmployees([...employees])
     }
 
-    // sorts the employees based on age.
     function sortByAge(){
         employees.sort(function(a,b){
             return (a.dob.age - b.dob.age)
