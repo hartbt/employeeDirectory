ae27ee42c3f6bfa539346a91aefdcd7db9256af4
Brendon Hart <brendonhartbusiness@gmail.com>
2020-12-08 23:37:13 -0700

    Added styling elements to multiple pages

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
+import Navbar from "./components/navbar"
+import Table from "./components/table"
+import { EmployeeContext } from "./components/employeeContext"
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+  const [displayedEmployees, setDisplayedEmployees] = useState([])
+  
+  const Style = {
+    overallStyle: {
+      textAlign: "center",
+      display: "flex",
+      flexDirection: "column"
+    },
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={Style.overallStyle}>
+      <EmployeeContext.Provider value={{employees, setEmployees, displayedEmployees, setDisplayedEmployees}}>
+        <Navbar />
+        <Table />
+      </EmployeeContext.Provider>
     </div>
   );
 }
